51e7199aa4eb9d6d16e1fc852eb424b0e2e393b6
Pratik Salaskar <95956963+pratiksalaskar1997@users.noreply.github.com>
2022-01-24 14:26:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 
-#check whether cold is present or not:
-s = 'today its very cold'
 
-for i in s.split():
-    if i == 'cold':
-        print('cold is present')
-    else:
-        print('cold is not present')
 ========================================================
 # WAP to get present and absent eligibility:
 nm = input('Enter student name: ')
