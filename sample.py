b83730432cf85cd23d5d5d4f60f33fc802cf3116
Pratik Salaskar <95956963+pratiksalaskar1997@users.noreply.github.com>
2022-03-15 18:28:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,26 +1,4 @@
-#List Comprehension to Iterate through a String
 
-str1 = 'pratiksalaskar'
-print([i.split() for i in str1])
-
-print([i for i in range(0, 101) if i % 2 == 0])
-print([i**2 for i in range(0, 101)])
-print([i for i in range(0, 101) if (i % 2)/i ==
-
-
-print([x for x in range(2, 20) if all(x % y != 0 for y in range(2, x))])
-
-print([x for x in range(0, 101) if x % 2 == 0 and x % 5 == 0])
-
-a=[[1,2,5],
-   [3,4,6]]
-t=[]
-for i in range(len(a[0])):
-    l=[]
-    for j in range (len(a)):
-        l.append(a[j][i])
-    t.append(l)
-print(t)
 ===============================================================================
 #1.Python program to find uncommon elements from two Strings
 
